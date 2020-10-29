0117d67b5257abdf91d58ed1813a34637a989922
Lisa Hou <lqnhou@gmail.com>
2020-10-29 11:08:46 -0700

    added k6 tests

--- tests/script.js
+++ tests/script.js
@@ -5,8 +5,21 @@ import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
 const csvData = papaparse.parse(open('../urls.csv'), { header: false }).data;
 
 export let options = {
-  vus: 1000,
-  duration: '1m'
+  stages: [
+    { duration: '20s', target: 10 },
+    { duration: '10s', target: 10 },
+    { duration: '1m', target: 100 },
+    { duration: '10s', target: 100 },
+    { duration: '1m', target: 200 },
+    { duration: '10s', target: 200 },
+    { duration: '2m', target: 400 },
+    { duration: '10s', target: 400 },
+    { duration: '2m', target: 600 },
+    { duration: '10s', target: 600 },
+    { duration: '2m', target: 800 },
+    { duration: '10s', target: 800 },
+    { duration: '1m', target: 0}
+  ],
 };
 
 
